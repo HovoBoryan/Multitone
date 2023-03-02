22f4e2677866c34d04a280732437c9f0c5162a3c
Boryan Hovo <bonnyluxe777@mail.ru>
2023-03-02 23:37:30 +0400

    i made changes

--- Multitone.js
+++ Multitone.js
@@ -30,3 +30,4 @@ class Multiton {
       }
     }
   }
+  module.exports = Multiton
--- Tests.js
+++ Tests.js
@@ -1,5 +1,14 @@
-function testforaddObjects() {
+const Multiton = require('./Multitone')
 
-};
+function testforaddObjects() {
+let obj1 = new Multiton();
+//let obj2 = new Multiton();
+
+if(obj1) {
+    console.log("test for Multiton failed !!!... ");
+} else {
+    console.log("Test for Multitone passed .");
+}
+}
 
 testforaddObjects();

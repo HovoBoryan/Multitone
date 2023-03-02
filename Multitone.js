74509562693eab0e30f83b6a6c8fbcd0f7cda6ce
Boryan Hovo <bonnyluxe777@mail.ru>
2023-03-02 23:10:34 +0400

    I made changes

---
+++ Multitone.js
@@ -0,0 +1,32 @@
+class Multiton {
+    static #instance = null;
+    static #limit = 4;
+    static #count = 0;
+    static #instanceArray = [];
+    static #fl = false;
+    static createInstance() {
+      this.#fl = true;
+  
+      this.#count++;
+  
+      console.log(this.#count);
+  
+      if (this.#count <= this.#limit) {
+        this.#instance = new Multiton();
+        this.#instanceArray.push(this.#instance);
+      }
+      if (this.count % this.limit === 0) {
+        return this.#instanceArray[this.#limit - 1];
+      } else {
+        return this.#instanceArray[(this.#count % this.#limit) - 1];
+      }
+  
+      this.#fl = false;
+    }
+  
+    constructor() {
+      if (!Multiton.#fl) {
+        throw new TypeError("PrivateConstructor is not constructable");
+      }
+    }
+  }
